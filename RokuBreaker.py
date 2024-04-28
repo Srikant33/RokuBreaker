e2e8883ad5a3fed46720984f3883b55c85e91dd4
srikant33 <ksrikantiyer33@gmail.com>
2024-04-27 19:20:07 -0500

    Adding check for keyboard

--- RokuBreaker.py
+++ RokuBreaker.py
@@ -2,10 +2,27 @@ import subprocess
 import time
 import keyboard
 
+
+def is_keyboard_open():
+    # Run the 'dumpsys input_method' command via adb
+    command = "adb shell dumpsys input_method"
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+
+    # Check if the output contains the 'mInputShown=true' string
+    return b"mInputShown=true" in output
+
+
 def send_key_event(key):
     # # Command to send key event via adb
     # command = f"adb shell input keyevent {key_event}"
-
+    if is_keyboard_open():
+        pass
+        # print("Keyboard is open.")
+    else:
+        while (not is_keyboard_open()):
+            time.sleep(0.1)
+        print("Keyboard was closed.")
     print("")
     for i in range(4):    
         print(key[i])
